ebad6219626f4b5baf59cf921d9943dc47b7d23c
shanqiu <68095814+shanqiiu@users.noreply.github.com>
2026-04-25 12:03:21 +0800

    Update __init__.py

--- ui_semantic_patch/app/renderers/__init__.py
+++ ui_semantic_patch/app/renderers/__init__.py
@@ -1,1 +1,18 @@
 
+# 渲染器模块
+# 负责生成各类异常 UI 效果
+
+from .base import BaseRenderer, RenderResult
+from .area_loading import AreaLoadingRenderer
+from .content_duplicate import ContentDuplicateRenderer
+from .patch import PatchRenderer
+from .text_overlay import TextOverlayRenderer
+
+__all__ = [
+    "BaseRenderer",
+    "RenderResult",
+    "AreaLoadingRenderer",
+    "ContentDuplicateRenderer",
+    "PatchRenderer",
+    "TextOverlayRenderer",
+]
